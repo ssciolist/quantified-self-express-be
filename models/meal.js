568412d544e1257377860a5acb8fb225abfc2aec
Megan Arellano <ssciolist@gmail.com>
2018-07-01 12:24:00 -0600

    creates meals router and adds it to app.js with basic /meals endpoint

--- models/meal.js
+++ models/meal.js
@@ -8,35 +8,6 @@ const all = () => {
   );
 };
 
-const create = (attrs) => {
-  return database.raw(
-    'INSERT INTO foods (name, calories) VALUES (?, ?) RETURNING id, name, calories;',
-    [attrs.name, attrs.calories]
-  );
-}
-
-const find = (food_id) => {
-  let id = food_id;
-  return database.raw(
-    'SELECT foods.id, foods.name, foods.calories FROM foods WHERE foods.id = ?', id
-  );
-}
-
-const update = (food_id, attrs) => {
-  let id = food_id;
-  return database.raw(
-    'UPDATE foods SET name = ?, calories = ? WHERE id = ? RETURNING *',
-    [attrs.name, attrs.calories, id]
-  );
-}
-
-const destroy = (food_id) => {
-  let id = food_id;
-  return database.raw(
-    'DELETE FROM foods WHERE foods.id = ?', id
-  );
-}
-
 module.exports = {
   all, create, find, update, destroy
 }
---
+++ routes/meals.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const Meal = require('../models/meal')
+
+/* GET all foods page. */
+router.get('/', (req, res) => {
+  Meal.all()
+    .then((data) => {
+      res.status(201).send(data)
+    })
+});
+
+module.exports = router;
