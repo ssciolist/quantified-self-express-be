2eaed4ee0752b517f4f36a42c57c76ccd68133b1
Megan Arellano <ssciolist@gmail.com>
2018-06-30 20:14:21 -0600

    modifies knexfile to connect to proper database. From there, deleted the mistake migration that was grandfathered into my db and renamed mealfoods migration to better match configuration. Finally, fix the seeding files in dev.

--- db/migrations/20180630174041_create_meal_foods.js
+++ db/migrations/20180630174041_create_meal_foods.js
@@ -4,8 +4,8 @@ exports.up = function(knex, Promise) {
     knex.schema
     .createTable('meal_foods', function(meal_foods) {
       meal_foods.increments('id').primary();
-      meal_foods.integer('meal_id').references('id').inTable('meals').notNull().onDelete('cascade')
-      meal_foods.integer('food_id').references('id').inTable('foods').notNull().onDelete('cascade')
+      meal_foods.integer('meal_id').references('id').inTable('meals').notNull()
+      meal_foods.integer('food_id').references('id').inTable('foods').notNull()
     })
   ])
 };
--- db/seeds/dev/meals.js
+++ db/seeds/dev/meals.js
@@ -1,20 +1,21 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('TRUNCATE meals RESTART IDENTITY')
+  return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
     .then(function() {
-      return Promise.all({
-        knew.raw(
+      return Promise.all([
+        knex.raw(
           'INSERT INTO meals (name) VALUES (?)', ["Breakfast"]
         ),
-        knew.raw(
+        knex.raw(
           'INSERT INTO meals (name) VALUES (?)', ["Snack"]
         ),
-        knew.raw(
+        knex.raw(
           'INSERT INTO meals (name) VALUES (?)', ["Lunch"]
         ),
-        knew.raw(
+        knex.raw(
           'INSERT INTO meals (name) VALUES (?)', ["Dinner"]
         )
-      })
+      ])
     })
+  }
