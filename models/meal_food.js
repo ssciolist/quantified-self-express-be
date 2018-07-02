c4277974ffef84569a5ae4a463e934ad813a001d
Megan Arellano <ssciolist@gmail.com>
2018-07-02 14:13:42 -0600

    file cleanup pry

--- models/meal_food.js
+++ models/meal_food.js
@@ -1,7 +1,6 @@
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
-pry = require('pryjs')
 
 const create = (meal_id, food_id) => {
   return database('meal_foods').insert({meal_id: meal_id, food_id: food_id}).returning('*')
--- routes/meals.js
+++ routes/meals.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-pry = require('pryjs')
+
 const Meal = require('../models/meal')
 const MealFood = require('../models/meal_food')
 
