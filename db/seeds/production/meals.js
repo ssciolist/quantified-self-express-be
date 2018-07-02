5c7a83021f05957bc2718714c30aabcbe7a92f43
Megan Arellano <ssciolist@gmail.com>
2018-07-02 14:41:56 -0600

    updates seed files

--- db/seeds/production/meals.js
+++ db/seeds/production/meals.js
@@ -4,17 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
     .then(function() {
       return Promise.all({
-        knex.raw(
-          'INSERT INTO meals (name) VALUES (?)', ["Breakfast"]
-        ),
-        knex.raw(
-          'INSERT INTO meals (name) VALUES (?)', ["Snack"]
-        ),
-        knex.raw(
-          'INSERT INTO meals (name) VALUES (?)', ["Lunch"]
-        ),
-        knex.raw(
-          'INSERT INTO meals (name) VALUES (?)', ["Dinner"]
-        )
+        knex.raw('INSERT INTO meals (name) VALUES (?)', ["Breakfast"]),
+        knex.raw('INSERT INTO meals (name) VALUES (?)', ["Snack"]),
+        knex.raw('INSERT INTO meals (name) VALUES (?)', ["Lunch"]),
+        knex.raw('INSERT INTO meals (name) VALUES (?)', ["Dinner"])
       })
     })
