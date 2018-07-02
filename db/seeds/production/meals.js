71465d43cf47c579f38112ff893b50c44c26c468
Megan Arellano <ssciolist@gmail.com>
2018-07-02 14:47:59 -0600

    syntax fix for seed

--- db/seeds/production/meals.js
+++ db/seeds/production/meals.js
@@ -3,10 +3,10 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
     .then(function() {
-      return Promise.all({
+      return Promise.all([
         knex.raw('INSERT INTO meals (name) VALUES ?', "Breakfast"),
         knex.raw('INSERT INTO meals (name) VALUES ?', "Snack"),
         knex.raw('INSERT INTO meals (name) VALUES ?', "Lunch"),
         knex.raw('INSERT INTO meals (name) VALUES ?', "Dinner")
-      })
+      ])
     })
