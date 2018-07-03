993c5a5f9f8d2de22e2dd6fa8e30277ab5cb6fe3
Megan Arellano <ssciolist@gmail.com>
2018-07-02 18:46:16 -0600

    installs chai, changes chai assert and expect imports

--- test/get_all_foods.js
+++ test/get_all_foods.js
@@ -1,10 +1,12 @@
-process.env.NODE_ENV = 'test';
+const environment = process.env.NODE_ENV = 'test';
 
 const chai = require('chai')
-const configuration = require('./knexfile')[environment]
+const assert = chai.assert
+const expect = chai.expect
+const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
-import { assert, expect } from 'chai';
+
 
 describe('GET /api/v1/foods', () => {
   it('it should get all the foods', (done) => {
