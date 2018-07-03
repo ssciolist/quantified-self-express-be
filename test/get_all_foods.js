ab228337ba1b3bb48b79f9250a7988569ad9b8d7
Megan Arellano <ssciolist@gmail.com>
2018-07-02 19:12:06 -0600

    sets up database cleaner and first assert

--- test/get_all_foods.js
+++ test/get_all_foods.js
@@ -1,15 +1,42 @@
 const environment = process.env.NODE_ENV = 'test';
+const app = require('../app');
 
-const chai = require('chai')
-const assert = chai.assert
-const expect = chai.expect
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should
+const expect = chai.expect
+chai.use(chaiHttp)
 
+describe('routes : foods', () => {
+  beforeEach((done) => {
+    database.migrate.latest()
+    .then(() => {
+      database.seed.run()
+      .then(() => {
+        done();
+      })
+    });
+  });
 
-describe('GET /api/v1/foods', () => {
-  it('it should get all the foods', (done) => {
-    c
-  })
-})
+  afterEach((done) => {
+    database.migrate.rollback()
+    .then(() => {
+      done();
+    });
+  });
+
+  describe('GET /api/v1/foods', () => {
+    it('should respond with all foods', (done) => {
+      chai.request(app)
+      .get('/api/v1/foods')
+      .end((err, res) => {
+        should.not.exist(err)
+        done()
+      });
+    });
+  });
+
+});
